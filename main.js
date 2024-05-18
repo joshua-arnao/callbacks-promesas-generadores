ed0c678e5c3bca637dc610a50fa4876fbcac736c
joshua-arnao <joshua.arnao@icloud.com>
2024-05-18 09:00:40 -0500

    Project Start

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -13,12 +12,9 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+     
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+console.log('Hola Mundo');
